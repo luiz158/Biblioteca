41c09cc9abb29a02bc020d9ad53bb8271b47d021
Marcelo <marcelostadtlober@gmail.com>
2013-01-29 21:29:05 -0200

    Commit inicial do projeto com eGit.

---
+++ Biblioteca/src/org/biblioteca/Pessoa.java
@@ -0,0 +1,30 @@
+package org.biblioteca;
+
+public class Pessoa {
+	//Atributos		
+	private String nome;		//Nome da pessoa
+	private int maxLivros;		//Número máximo de livros que a pessoa pode emprestar
+	
+	//Construtores
+	public Pessoa() {
+		nome = "Nome desconhecido";
+		maxLivros = 3;
+	}
+	
+	//Métodos
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getMaxLivros() {
+		return maxLivros;
+	}
+
+	public void setMaxLivros(int maxLivros) {
+		this.maxLivros = maxLivros;
+	}
+}
