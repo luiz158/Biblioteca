fb727bbd31e0a17d2741e8106b00987b335b6af7
Marcelo <marcelostadtlober@gmail.com>
2013-02-01 15:54:58 -0200

    Criação de JUnitTests

---
+++ Biblioteca/test/org/biblioteca/PessoaTest.java
@@ -0,0 +1,24 @@
+package org.biblioteca;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PessoaTest {
+
+	@Test
+	public void testPessoa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaxLivros() {
+		fail("Not yet implemented");
+	}
+
+}
