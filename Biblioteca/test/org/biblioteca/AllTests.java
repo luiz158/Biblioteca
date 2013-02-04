2f17d7162b66c1fcca7024874321335e28b5718d
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2013-02-04 19:53:14 -0200

    Criação de JUnit Test Suit.

---
+++ Biblioteca/test/org/biblioteca/AllTests.java
@@ -0,0 +1,11 @@
+package org.biblioteca;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LivroTest.class, PessoaTest.class })
+public class AllTests {
+
+}
