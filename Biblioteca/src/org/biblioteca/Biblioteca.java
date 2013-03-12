7059e2570e900eafe56eabf004448ed1149be66f
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2013-03-12 16:52:08 -0300

    Finalização das Classes Biblioteca e BibliotecaTest

--- Biblioteca/src/org/biblioteca/Biblioteca.java
+++ Biblioteca/src/org/biblioteca/Biblioteca.java
@@ -15,4 +15,25 @@ public class Biblioteca {
 		pessoas = new ArrayList<Pessoa>();
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public ArrayList<Livro> getLivros() {
+		return livros;
+	}
+
+	public ArrayList<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void addLivro(Livro l1) {
+		this.livros.add(l1);
+	}
+
+	public void removeLivro(Livro l1) {
+		this.livros.remove(l1);		
+		
+	}
+
 }
--- Biblioteca/test/org/biblioteca/BibliotecaTest.java
+++ Biblioteca/test/org/biblioteca/BibliotecaTest.java
@@ -5,6 +5,13 @@ import java.util.ArrayList;
 import junit.framework.TestCase;
 
 public class BibliotecaTest extends TestCase {
+	
+	private Livro l1;
+	private Livro l2;
+	private Pessoa p1;
+	private Pessoa p2;
+	private Biblioteca b1;
+
 	//Teste do Construtor
 	
 	public void testBiblioteca() {
@@ -12,7 +19,42 @@ public class BibliotecaTest extends TestCase {
 		assertEquals("Unicentro", b1.nome);
 		assertTrue(b1.livros instanceof ArrayList);
 		assertTrue(b1.pessoas instanceof ArrayList);
+	}
+	
+	public void setup() {
+		l1 = new Livro("Livro1");
+		l2 = new Livro("Livro2");
+		p1 = new Pessoa();
+		p2 = new Pessoa();
 		
-		b1.livros.add("teste");
+		p1.setNome("Bart");
+		p2.setNome("Lisa");
+		
+		b1 = new Biblioteca("Teste");
+	}
+	
+	public void testAddLivro() {
+		// Cria os objetos
+		setup();
+		
+		// Testa se a Biblioteca esta vazia
+		assertEquals(0, b1.getLivros().size());
+		
+		b1.addLivro(l1);
+		b1.addLivro(l2);
+		
+		assertEquals(2, b1.getLivros().size());
+		assertEquals(0, b1.getLivros().indexOf(l1));
+		assertEquals(1, b1.getLivros().indexOf(l2));
+		
+		b1.removeLivro(l1);
+		
+		assertEquals(1, b1.getLivros().size());
+		assertEquals(0, b1.getLivros().indexOf(l2));
+		
+		b1.removeLivro(l2);
+		
+		assertEquals(0, b1.getLivros().size());
+		
 	}
 }
